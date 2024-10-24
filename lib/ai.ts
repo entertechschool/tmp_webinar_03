fd6dd9417041de21c4c98dbb7ce0ef5a01d0948e
bdiazc90 <bdiazc90@gmail.com>
2024-10-23 21:40:04 -0500

    comment openai

--- lib/ai.ts
+++ lib/ai.ts
@@ -1,28 +1,28 @@
-import OpenAI from "openai";
-const openai = new OpenAI();
+// import OpenAI from "openai";
+// const openai = new OpenAI();
 
-const aiModel = "gpt-4o-mini";
+// const aiModel = "gpt-4o-mini";
 
-async function summarizeNote(rawNote: string) {
-    return await openai.chat.completions.create({
-        model: aiModel,
-        messages: [
-            { role: "system", content: "Eres un excelente analista de comunicaciones, y puedes resumir textos de forma muy eficiente." },
-            {
-                role: "user",
-                content: "Realiza un resumen super corto de la siguiente nota: " + rawNote,
-            },
-        ],
-    });
-}
+// async function summarizeNote(rawNote: string) {
+//     return await openai.chat.completions.create({
+//         model: aiModel,
+//         messages: [
+//             { role: "system", content: "Eres un excelente analista de comunicaciones, y puedes resumir textos de forma muy eficiente." },
+//             {
+//                 role: "user",
+//                 content: "Realiza un resumen super corto de la siguiente nota: " + rawNote,
+//             },
+//         ],
+//     });
+// }
 
-const ai = {
-    aiModel,
-    summarizeNote
-}
-
-
-export default ai;
+// const ai = {
+//     aiModel,
+//     summarizeNote
+// }
 
 
-// console.log(completion.choices[0].message);
+// export default ai;
+
+
+// // console.log(completion.choices[0].message);

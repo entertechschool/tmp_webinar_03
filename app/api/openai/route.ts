5ebf094e78a68a4b23ed4ccda45c0c4820934394
bdiazc90 <bdiazc90@gmail.com>
2024-10-23 21:34:56 -0500

    fix typing

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,10 +1,10 @@
-import ai from "@/lib/ai";
+// import ai from "@/lib/ai";
 
 export async function POST(request: Request) {
     const { rawNote } = await request.json();
-    // const summarizeNote = "Esta es tu nota resumida: " + rawNote;
-    const completion = await ai.summarizeNote(rawNote);
-    const summarizeNote = completion.choices[0].message.content;
+    const summarizeNote = "Esta es tu nota resumida: " + rawNote;
+    // const completion = await ai.summarizeNote(rawNote);
+    // const summarizeNote = completion.choices[0].message.content;
     return Response.json({data: summarizeNote});
 
     // const completion = await ai.summarizeNote(rawNote);
